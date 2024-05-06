9054b09ccb313a17169c2b07690ac2c4f0da3f81
Валентин <123qwemmm@mail.ru>
2024-05-06 15:17:54 +0300

    доработал координаты чтобы в правый нижний угол вставлять

--- image_to_image.py
+++ image_to_image.py
@@ -1,40 +1,51 @@
 from PIL import Image
 
 # Откуда брать изображения
-old_patch = '' # Пример 'first/second/' слеш не может быть вначале или один
+path_in_file = "d:/old/"  # "d:/2222222/"
+name_in_file = "old - "  # "S2360033.MP4"
 
 # Где лежит накладываемое изображение
-overlay_path = ''
+img_insert = Image.open('c:/Users/valstan/Downloads/Ознакомлен Симонов печать.png')
+
+# Получить размеры вставляемого изображения
+img_insert_width, img_insert_height = img_insert.size
 
 # Куда сохранять новые изображения
-new_patch = ''
+path_out_file = "d:/rezult/"
+name_out_file = "out_video.mp4"
 
 # Расширение файлов, например '.png'
-file_ext = ''
+file_ext = '.tif'
 
 # Смещение в пикселях по ширине от левого края
-left_offset = 0
-right_offset = 0
+left_offset = 10
+right_offset = 10
 
-img = Image.open('00001111.png')
 
-name = "24.03.15 №60 КОН.АНАЛИЗ - "
-
-for a in range(1,441):
+for a in range(1,138):  # На единицу больше чем число изображений
     if a == 0:
         continue
-    name_image = name + '000' + str(a)
+    name_image = name_in_file + '000' + str(a)
     if 10 <= a < 100:
-        name_image = name + '00' + str(a)
+        name_image = name_in_file + '00' + str(a)
     elif 100 <= a < 1000:
-        name_image = name + '0' + str(a)
+        name_image = name_in_file + '0' + str(a)
 
-    old_image = Image.open(old_patch + name_image + file_ext)
+    old_image = Image.open(path_in_file + name_image + file_ext)
+
+    # Получить размеры изображения
+    old_image_width, old_image_height = old_image.size
+
+    # Координаты правого нижнего угла
+    right_bottom = (old_image_width - 1, old_image_height - 1)
 
     # Смещение в пикселях от левого верхнего угла
     # два раза img это чтобы прозрачность сработала
-    old_image.paste(img, (1420, 2960), img)
-    old_image.save('5678/' + name_image + '.png')
+    old_image.paste(img_insert,
+                    (old_image_width - img_insert_width - left_offset,
+                     old_image_height- img_insert_height - right_offset),
+                    img_insert)
+    old_image.save(path_out_file + name_image + file_ext)
     old_image.close()
 
     print(name_image)
